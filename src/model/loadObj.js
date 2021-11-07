5c6cc86eccb046432a48e8ae2cf183ee46728a48
youmyhope <luuhuong196@gmail.com>
2021-11-07 19:26:39 +0300

    remove node_modules

--- src/model/loadObj.js
+++ src/model/loadObj.js
@@ -1,5 +1,5 @@
-import * as THREE from '../../node_modules/three/build/three.module.js';
-import {OBJLoader} from '../../node_modules/three/examples/jsm/loaders/OBJLoader.js';
+import * as THREE from '../three/build/three.module.js';
+import {OBJLoader} from '../three/examples/jsm/loaders/OBJLoader.js';
 
 export default function (scene){
     const loader = new OBJLoader();
