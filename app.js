f64399e32e4403bf343507d5654f4a905190d6a9
youmyhope <luuhuong196@gmail.com>
2021-11-07 19:59:43 +0300

    Final

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express()
 app.use(express.static(__dirname + '/src'))
 
 app.listen(3000, () =>
-	console.log('Visit http://127.0.0.1:3000')
+	console.log('Сайт будет размещен по адресу http://127.0.0.1:3000')
 );
